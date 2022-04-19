e7974eba64daeb6fb39c2ef4fcbefd7d49b49b32
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-19 16:42:48 +0800

    fix: remove comments

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -1,17 +1,11 @@
 const Character = require("../models/characte.model");
 const { createError } = require("../utils/createError");
 
-// @desc    List characters
-// @route   GET /api/characters
-// @access  Private
 const index = async (req, res) => {
 	const characters = await Character.find().sort({ createdAt: -1 });
 	res.status(200).json(characters);
 };
 
-// @desc    Single character
-// @route   GET /api/characters/:id
-// @access  Private
 const show = async (req, res, next) => {
 	const id = req.params.id;
 
@@ -25,18 +19,12 @@ const show = async (req, res, next) => {
 	// res.render("details", { title: "Details", character });
 };
 
-// @desc    Set character
-// @route   POST /api/characters
-// @access  Private
 const store = async (req, res, next) => {
 	const character = await Character.create({ ...req.body });
 	// res.redirect("/characters");
 	res.status(200).json(character);
 };
 
-// @desc    Update character
-// @route   PUT /api/characters/:id
-// @access  Private
 const update = async (req, res, next) => {
 	const character = await Character.findByIdAndUpdate(
 		req.params.id,
@@ -49,9 +37,6 @@ const update = async (req, res, next) => {
 	res.status(200).json(character);
 };
 
-// @desc    Delete character
-// @route   DELETE /api/characters/:id
-// @access  Private
 const destroy = async (req, res) => {
 	const id = req.params.id;
 	await Character.findByIdAndDelete(id);
