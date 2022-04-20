10ae9928a06d384e82f023351feacfba6b2e1e23
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-20 18:26:21 +0800

    feat: added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorHandler");
 // routes
 const characterRoutes = require("./routes/character.routes");
@@ -9,6 +10,7 @@ const characterRoutes = require("./routes/character.routes");
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res, next) => {
 	res.status(200).json("welcome to hxh api");
