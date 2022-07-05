cfd4a513b1b0169c5a00efe96341b45e7ce8f10a
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-05 22:55:17 +0800

    Created AppError

---
+++ utils/appError.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+	constructor(message, statusCode, errors = null) {
+		super(message);
+
+		this.statusCode = statusCode;
+		this.status = `${statusCode}`.startsWith('4') ? 'failed' : 'error';
+		this.isOperational = true;
+		this.errors = errors;
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export default AppError;
