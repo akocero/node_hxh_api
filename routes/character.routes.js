2972f4ac5e0e0d26fbc50398c85384fa22c439b9
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-05 22:55:09 +0800

    Remove auth for Development purpose

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -13,8 +13,8 @@ import { auth } from '../middlewares/auth.js';
 
 router.get('/', index);
 router.get('/:id', catchUnknownError(show));
-router.put('/:id', auth, upload.single('image'), catchUnknownError(update));
-router.post('/', auth, upload.single('image'), catchUnknownError(store));
+router.put('/:id', upload.single('image'), catchUnknownError(update));
+router.post('/', upload.single('image'), catchUnknownError(store));
 router.delete('/:id', auth, catchUnknownError(destroy));
 
 export default router;
