7d06a726a16c1daf878e53c923145f0a9f94a143
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-06 22:29:53 +0800

    Implement Auth

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -9,12 +9,17 @@ import {
 } from '../controllers/character.controller.js';
 import { catchUnknownError } from '../middlewares/catchUnknownError.js';
 import upload from '../utils/multer.js';
-import { auth } from '../middlewares/auth.js';
+import auth from '../middlewares/auth.js';
 
-router.get('/', index);
-router.get('/:id', catchUnknownError(show));
-router.put('/:id', upload.single('image'), catchUnknownError(update));
-router.post('/', upload.single('image'), catchUnknownError(store));
-router.delete('/:id', auth, catchUnknownError(destroy));
+router.get('/', auth.protect, index);
+router.get('/:id', auth.protect, show);
+router.put(
+	'/:id',
+	auth.protect,
+	upload.single('image'),
+	catchUnknownError(update),
+);
+router.post('/', auth.protect, upload.single('image'), store);
+router.delete('/:id', auth.protect, auth.restrictedTo('admin'), destroy);
 
 export default router;
