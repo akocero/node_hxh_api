38302ac41b56c46c28573a904ee930d60d2fd63a
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-06 22:30:03 +0800

    Implement Auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 const router = Router();
 import { register, login, me } from '../controllers/user.controller.js';
-import { auth } from '../middlewares/auth.js';
+import auth from '../middlewares/auth.js';
 import { catchUnknownError } from '../middlewares/catchUnknownError.js';
 
 router.post('/register', catchUnknownError(register));
 router.post('/login', catchUnknownError(login));
-router.get('/me', auth, me);
+router.get('/me', auth.protect, me);
 
 export default router;
