3a2b2c4ba7120851c18d0a00ac2271d7bbfffb6d
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-06 22:29:43 +0800

    Added Role and CorrectPassowrd Method

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,40 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
 	{
 		name: {
 			type: String,
-			required: [true, 'Please add a name']
+			required: [true, 'Please add a name'],
 		},
 		email: {
 			type: String,
 			required: [true, 'Please add an email'],
 			unique: true,
-			validate: [validator.isEmail, 'Please add a valid email']
+			validate: [validator.isEmail, 'Please add a valid email'],
+		},
+		role: {
+			type: String,
+			enum: ['user'],
+			default: 'user',
 		},
 		password: {
 			type: String,
-			required: [true, 'Please add a password']
-		}
+			required: [true, 'Please add a password'],
+			select: false,
+		},
 	},
 	{
-		timestamps: true
-	}
+		timestamps: true,
+	},
 );
+
+userSchema.methods.correctPassword = async function (
+	candidatePassword,
+	userPassword,
+) {
+	return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 export default mongoose.model('User', userSchema);
