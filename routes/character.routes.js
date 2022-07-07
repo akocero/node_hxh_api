862018f0e8d404fbcb7cbbe1f74a765cba9149c8
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-07 19:44:49 +0800

    Implement Update Factory

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -7,18 +7,12 @@ import {
 	show,
 	destroy,
 } from '../controllers/character.controller.js';
-import { catchUnknownError } from '../middlewares/catchUnknownError.js';
 import upload from '../utils/multer.js';
 import auth from '../middlewares/auth.js';
 
 router.get('/', auth.protect, index);
 router.get('/:id', auth.protect, show);
-router.put(
-	'/:id',
-	auth.protect,
-	upload.single('image'),
-	catchUnknownError(update),
-);
+router.patch('/:id', auth.protect, upload.single('image'), update);
 router.post('/', auth.protect, upload.single('image'), store);
 router.delete('/:id', auth.protect, auth.restrictedTo('admin'), destroy);
 
