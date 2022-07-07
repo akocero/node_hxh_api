c4b65f6bbddaa24be92cc45ac2355280a9f95480
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-07 19:45:42 +0800

    Change Results Limit to 20

--- utils/queryBuilder.js
+++ utils/queryBuilder.js
@@ -45,7 +45,7 @@ class QueryBuilder {
 
 	paginate() {
 		const page = this.queryString.page * 1 || 1;
-		const limit = this.queryString.limit * 1 || 100;
+		const limit = this.queryString.limit * 1 || 20;
 		const skip = (page - 1) * limit;
 
 		this.query = this.query.skip(skip).limit(limit);
