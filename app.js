c7ed31cbb5261d537787db90c1be209c2a0e1a4c
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-07 19:42:13 +0800

    Removed Nopras Endpoints

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ import characterRouter from './routes/character.routes.js';
 import userRouter from './routes/user.routes.js';
 import familyRouter from './routes/family.routes.js';
 import groupRouter from './routes/group.routes.js';
-import templateRouter from './routes/template.routes.js';
-import orderRouter from './routes/order.routes.js';
-import customerRouter from './routes/customer.routes.js';
 
 // middlewares / utilities
 import AppError from './utils/appError.js';
@@ -35,9 +32,6 @@ app.use('/api/v1/characters', characterRouter);
 app.use('/api/v1/auth', userRouter);
 app.use('/api/v1/families', familyRouter);
 app.use('/api/v1/groups', groupRouter);
-app.use('/api/v1/orders', orderRouter);
-app.use('/api/v1/customers', customerRouter);
-app.use('/api/v1/templates', templateRouter);
 
 app.use((req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
